4659a41e36aca08933d6c495e3f94ef2abcbe820
Charlotte Chen <charlottechen21@gmail.com>
2016-08-23 09:47:40 -0400

    Debugged button form

--- widget.js
+++ widget.js
@@ -1,7 +1,7 @@
 var buttonClicked = true;
 $("button").click(function () {
     //if button is clicked
-    if(buttonClicked){
+    if(buttonClicked==true){
         $("form").slideDown("slow");
         buttonClicked=false;
     }
@@ -12,21 +12,37 @@ $("button").click(function () {
     }
 });
 
+var mouseleave = false;
 $("button").mouseover(function(){
+    $("#feedback-widget").css({
+        //circle to rect
+        'border-radius':'0', 
+        'width':'300px', 
+        'height':'50px'});
     $("i").hide();
     $("p").show();
+    
+    mouseleave = false;
 });
 
 $("button").mouseout(function() {
-    if (buttonClicked){
+    if (buttonClicked==true){
+        $("#feedback-widget").css({
+            //rect to circle
+            'width': '50px',
+            'height': '50px',
+            '-moz-border-radius': '50px',
+            '-webkit-border-radius': '50px',
+            'border-radius': '50px'});
         $("p").hide();
         $("i").show();
     } else {
         $("#feedback-widget").css({
+            //circle to rect
             'border-radius':'0', 
             'width':'300px', 
             'height':'50px'});
         $("p").show();
-        
     }
+    mouseleave = true;
 });
