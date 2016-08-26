c966dccca86db74e6b30ac9f945fcdaeaa6a76b1
584772 <robertmarriot@C02S222CFVH8.local>
2016-08-26 15:42:27 -0400

    updated analytics to include a time stamp

--- OMGCSSAnalytics.js
+++ OMGCSSAnalytics.js
@@ -26,15 +26,16 @@ var url_timedata = "http://54.183.84.147:3000/api/v1/analytics/time";
 //Empty object to return to DB
 var UserObject = {
 	"uuid": null, //Main identification information
-	"geolocationinfo": null
-	, "fingerprint": null
+	"geolocationinfo": null,
+	"fingerprint": null
 };
 
 var PageObject = {
 	"uuid": null, //Smaller object for time on page
-	"timeonpage": 0
-	, "docreferrer": ""
-	, "docURI": ""
+	"timeonpage": 0,
+  "docreferrer": "",
+	"docURI": "",
+	"timestamp": ""
 };
 
 var GeoObject = {}; //Output from the geolocation server.
@@ -44,7 +45,7 @@ var GeoObject = {}; //Output from the geolocation server.
 $(document).ready(function starterFunction() {
 	//Begin Time counter object from Timeme.js
 	TimeMe.setIdleDurationInSeconds(15);
-	TimeMe.setCurrentPageName("Analytics-OMGCSS");
+	TimeMe.setCurrentPageName(window.parent.location.href);
 	TimeMe.initialize();
 	//run asynchronous commands to get geo and ip information
 	getClientIPGEO(ipRequestServer, geoAPIkey);
@@ -57,6 +58,8 @@ $(document).ready(function starterFunction() {
 $(window).unload(function () {
 	//On window unload, fill out time on page and post objects to DB
 	PageObject.timeonpage = TimeMe.js.getTimeOnCurrentPageInSeconds();
+	PageObject.timestamp = getTimeStamp();
+
 	$.post(url_userdata, UserObject)
 	$.post(url_timedata, PageObject)
 });
@@ -94,6 +97,33 @@ function getFingerprint() {
 		console.log(components);
 		console.log(result);
 	});
+}
+
+/**
+ * Return a timestamp with the format "m/d/yy h:MM:ss TT"
+ * @type {Date}
+ */
+
+function getTimeStamp() {
+// Create a date object with the current time
+  var now = new Date();
+// Create an array with the current month, day and time
+  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+// Create an array with the current hour, minute and second
+  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+// Determine AM or PM suffix based on the hour
+  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+// Convert hour from military time
+  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+// If hour is 0, set it to 12
+  time[0] = time[0] || 12;
+// If seconds and minutes are less than 10, add a zero
+  for ( var i = 1; i < 3; i++ ) {
+    if ( time[i] < 10 ) {
+      time[i] = "0" + time[i];
+    }
+  }
+  return date.join("/") + " " + time.join(":") + " " + suffix;
 }
 
 //Creates a unique string for a UUID with reg expression. Different per page visit.
