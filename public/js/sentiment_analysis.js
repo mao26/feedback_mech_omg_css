a9b21ecfac0310c31324d76c0be3d86b6147cca0
583250 <mariooliver@C02RJ1QEFVH8.local>
2016-08-29 11:15:20 -0400

    formatting for the list of comments and sentiment analysis added

--- public/js/sentiment_analysis.js
+++ public/js/sentiment_analysis.js
@@ -1,3 +1,11 @@
 var sentiment = require('sentiment');
-var r1 = sentiment('Cats are stupid');
-console.dir(r1);
+console.log("hello sentiment");
+$.getJSON("http://54.183.84.147:3000/api/v1/feedback", function (data) {
+    var i;
+    var array = data;
+    for (i = 0; i < array.length; i++) {
+        var sent_for_object = sentiment(array[i].feedback);
+        console.log(sent_for_object);
+        console.log(array[i].reaction);
+    }
+});
